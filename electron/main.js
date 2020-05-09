ffe57ca6581311d84f4b01a1a1b718b34fa40b37
J.S.Patrick <980141374@qq.com>
2020-05-09 19:22:20 +0800

    electron init！

--- electron/main.js
+++ electron/main.js
@@ -4,11 +4,17 @@ let windowConfig = {
 	width:800,
 	height:600
 };
+
+if(process.argv[1] === '.'){
+	console.log('serve');
+}
 // 窗口配置程序运行窗口的大小
 function createWindow(){
 	win = new BrowserWindow(windowConfig);// 创建一个窗口
 	// todo 若是本地开发，此处应当是localhost
-	win.loadURL(`file://${__dirname}/index.html`);// 在窗口内要展示的内容index.html 就是打包生成的index.html
+	
+	// win.loadURL(`file://${__dirname}/index.html`);// 在窗口内要展示的内容index.html 就是打包生成的index.html
+	win.loadURL('http://localhost:8080')
 	win.webContents.openDevTools();  // 开启调试工具
 	win.on('close',() => {
 		// 回收BrowserWindow对象
